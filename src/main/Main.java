031e68787e3f13687e38b9a65ce18f723a0f94cf
Mike <ogar.m.v@mail.ru>
2024-04-19 16:08:47 +0300

    Main commit

---
+++ src/main/Main.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Student studentOne = new Student(
+                "Vadim", 32, 89676400941l, "sotnikov.v.v@mail.ru");
+
+        Student studentTwo = new Student(
+                "Alex", 39, 89676400942l, "rybyhin.a.v@mail.ru");
+
+        Student studentThree = new Student(
+                "Seva", 30, 89676400943l, "ostrovscki.v.a@mail.ru");
+
+        Student studentFour = new Student(
+                "Rad", 14, 89676400944l, "juravlev.r.k@mail.ru");
+
+        Student studentFive = new Student(
+                "Sasha", 20, 89676400945l, "karpinskiy.v.v@mail.ru");
+
+        Student studentSix = new Student(
+                "Ulyana", 16, 89676400946l, "frolova.u.i@mail.ru");
+
+        Student studentSeven = new Student(
+                "Dima", 17, 89676400947l, "minosyan.d.v@mail.ru");
+
+        Student studentEight = new Student(
+                "Andry", 16, 89676400948l, "grigorov.a.g@mail.ru");
+
+        Student studentNine = new Student(
+                "Andry", 16, 89676400948l, "grigorov.a.g@mail.ru");
+
+        Student studentTen = new Student(
+                "Mike", 23, 89676400948l, "muramail.ru");
+
+        Student studentEleven = new Student(
+                "Gregory", 26, 896764009481111111l, "avosin@mail.ru");
+
+//        Client clientTwelve = null;
+//        System.out.println(clientTwelve.toString());
+
+        while (true) {
+            System.out.println("Input on the console command \"PrintInformationAboutClients\", \"Remove\":");
+            String command = new Scanner(System.in).nextLine();
+            if (command.equals("PrintInformationAboutClients")) {
+                Student.printInformationAboutStudents();
+            } else if (command.equals("Remove")) {
+                System.out.println("Input criterion for remove client: ");
+                long number = new Scanner(System.in).nextLong();
+                Student.removeStudent(number);
+            } else if (command.equals("GetCountStudents")) {
+                System.out.println(Student.getCountStudents());
+            }
+        }
+    }
+}
---
+++ src/main/Student.java
@@ -0,0 +1,87 @@
+
+package main;
+
+import java.util.*;
+
+public class Student{
+    private static int count = 0;
+    private String name; //Mike
+    private int age; //23
+    private String email; //mura.m.v@email.ru
+    private long phoneNumber; //89676400941
+    private static ArrayList<Student> setStudents = new ArrayList<>();
+
+    public Student(String name, int age, long phoneNumber, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        setStudents.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && phoneNumber == student.phoneNumber && Objects.equals(name, student.name) && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, email, phoneNumber);
+    }
+
+    public static void printInformationAboutStudents() {
+        //Evgeniy
+    }
+
+    public static void removeStudent(long phoneNumber) {
+        //Dima
+    }
+
+    public static int getCountStudents() {
+        //Misha >
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Name \"" + getName() + "\"," +
+                "\nAge \"" + getAge() + "\"," +
+                "\nEmAil \"" + getEmail() + "\"," +
+                "\nPhone number " + getPhoneNumber() + ".\n";
+    }
+}
